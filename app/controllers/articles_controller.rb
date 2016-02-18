7ff2e6b33818d0f395a847e6e65836f22345e5ae
Austin <abenfield@protonmail.com>
2016-02-18 02:30:33 +0000

    Added basic functionality.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,58 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+   def index
+      @articles = Article.all 
+   end
+
+    def destroy
+
+@article.destroy
+flash[:notice] = "Article was successfully deleted"
+redirect_to articles_path
+    end
+   
     def new
     @article = Article.new
     end
+    
+    def edit
 
+    end
+    
 def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+   
+    if @article.save
+      flash[:notice] = "Article was saved!"
+  redirect_to article_path(@article)
+   else
+       render 'new'
+        
+    end
 end
 
+def update
+
+if @article.update(article_params)
+flash[:notice] = "Article was successfully updated"
+redirect_to article_path(@article)
+else
+render 'edit'
+end
+end
+
+
+
+def show
+   
+end
     private
-    def article_params
+   
+   def set_article
+      @article = Article.find(params[:id]) 
+   end
+   
+   def article_params
 params.require(:article).permit(:title, :description)
     end
     
